7178a9e992412db4b486087ae19e2419f6d86f90
Roel-Janssen-44 <roeljanssen2002@gmail.com>
2023-12-24 14:18:37 +0100

    removed product descriptionHTML

--- app/components/CollectionCollage.js
+++ app/components/CollectionCollage.js
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { Image } from "@shopify/hydrogen-react";
-import DOMPurify from "dompurify";
+// import DOMPurify from "dompurify";
 import Button from "@mui/material/Button";
 
 export default function CollectionCollage({ collections }) {
@@ -12,9 +12,8 @@ export default function CollectionCollage({ collections }) {
     <div className="mt-20 container">
       {Object.keys(collections).map((key) => {
         const collection = collections[key];
-        const htmlContent = collection.descriptionHtml;
-
-        const sanitizedHtmlContent = DOMPurify.sanitize(htmlContent);
+        // const htmlContent = collection.descriptionHtml;
+        // const sanitizedHtmlContent = DOMPurify.sanitize(htmlContent);
 
         return (
           <div
@@ -26,10 +25,10 @@ export default function CollectionCollage({ collections }) {
               <hr className="bg-primary rounded h-[3px] border-none w-16" />
               <h2 className="text-xl ">Collection {collection.title}</h2>
             </div>
-            <p
+            {/* <p
               className="mb-4"
               dangerouslySetInnerHTML={{ __html: sanitizedHtmlContent }}
-            ></p>
+            ></p> */}
 
             <Link href={"/"} className="mb-6 block">
               <Button
