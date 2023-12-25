13f1b3d4f434e5a6a672c7f735a7bff9f5df6124
Roel-Janssen-44 <roeljanssen2002@gmail.com>
2023-12-25 10:01:40 +0100

    fixed html sanitizer

--- app/components/CollectionCollage.js
+++ app/components/CollectionCollage.js
@@ -1,19 +1,22 @@
 "use client";
 
+import sanitizeHtml from "sanitize-html-react";
+
 import Link from "next/link";
 import { Image } from "@shopify/hydrogen-react";
-// import DOMPurify from "dompurify";
 import Button from "@mui/material/Button";
 
 export default function CollectionCollage({ collections }) {
   console.log("collections");
   console.log(collections);
+
   return (
     <div className="mt-20 container">
       {Object.keys(collections).map((key) => {
         const collection = collections[key];
         // const htmlContent = collection.descriptionHtml;
         // const sanitizedHtmlContent = DOMPurify.sanitize(htmlContent);
+        const sanitizedHtmlContent = sanitizeHtml(collection.descriptionHtml);
 
         return (
           <div
@@ -25,16 +28,16 @@ export default function CollectionCollage({ collections }) {
               <hr className="bg-primary rounded h-[3px] border-none w-16" />
               <h2 className="text-xl ">Collection {collection.title}</h2>
             </div>
-            {/* <p
+            <p
               className="mb-4"
               dangerouslySetInnerHTML={{ __html: sanitizedHtmlContent }}
-            ></p> */}
+            ></p>
 
             <Link href={"/"} className="mb-6 block">
               <Button
                 variant="contained"
                 size="large"
-                className="normal-case font-normal"
+                className="bg-primary normal-case font-normal"
               >
                 Ontdekken
               </Button>
